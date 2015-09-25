e4e417c616756e21c96cb2293758bf0f0008b62c
Yoel Monzon <yoelfme@hotmail.com>
2015-09-25 00:50:54 -0600

    [add] ground loop

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -1,11 +1,19 @@
 import Foundation
 
 class MainScene: CCNode {
-    weak var hero: CCSprite!
+    weak var hero : CCSprite!
+    weak var gamePhysicsNode : CCPhysicsNode!
+    weak var ground1 : CCSprite!
+    weak var ground2 : CCSprite!
+    
     var sinceTouch : CCTime = 0
+    var scrollSpeed : CGFloat = 80
+    var grounds = [CCSprite]()  // initializes an empty array
     
     func didLoadFromCCB() {
         userInteractionEnabled = true
+        grounds.append(ground1)
+        grounds.append(ground2)
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
@@ -27,6 +35,19 @@ class MainScene: CCNode {
         if (sinceTouch > 0.3) {
             let impulse = -18000.0 * delta
             hero.physicsBody.applyAngularImpulse(CGFloat(impulse))
+        }
+        
+        hero.position = ccp(hero.position.x + scrollSpeed * CGFloat(delta), hero.position.y)
+        
+        gamePhysicsNode.position = ccp(gamePhysicsNode.position.x - scrollSpeed * CGFloat(delta), gamePhysicsNode.position.y)
+        
+        // loop the ground whenever a ground image was moved entirely outside the screen
+        for ground in grounds {
+            let groundWorldPosition = gamePhysicsNode.convertToWorldSpace(ground.position)
+            let groundScreenPosition = convertToNodeSpace(groundWorldPosition)
+            if groundScreenPosition.x <= (-ground.contentSize.width) {
+                ground.position = ccp(ground.position.x + ground.contentSize.width * 2, ground.position.y)
+            }
         }
     }
 }
