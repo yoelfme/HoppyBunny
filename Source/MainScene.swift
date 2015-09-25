5ed6a6ccdd6a97ddb483d397b0a1ebf4f0e38782
Yoel Monzon <yoelfme@hotmail.com>
2015-09-25 00:04:44 -0600

    [add] project tutorial part 3

---
+++ Source/MainScene.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class MainScene: CCNode {
+    weak var hero: CCSprite!
+    var sinceTouch : CCTime = 0
+    
+    func didLoadFromCCB() {
+        userInteractionEnabled = true
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        hero.physicsBody.applyImpulse(ccp(0, 400))
+        hero.physicsBody.applyAngularImpulse(10000)
+        sinceTouch = 0
+    }
+    
+    override func update(delta: CCTime) {
+        let velocityY = clampf(Float(hero.physicsBody.velocity.y), -Float(CGFloat.max), 200)
+        hero.physicsBody.velocity = ccp(0, CGFloat(velocityY))
+        
+        sinceTouch += delta
+        hero.rotation = clampf(hero.rotation, -30, 90)
+        if (hero.physicsBody.allowsRotation) {
+            let angularVelocity = clampf(Float(hero.physicsBody.angularVelocity), -2, 1)
+            hero.physicsBody.angularVelocity = CGFloat(angularVelocity)
+        }
+        if (sinceTouch > 0.3) {
+            let impulse = -18000.0 * delta
+            hero.physicsBody.applyAngularImpulse(CGFloat(impulse))
+        }
+    }
+}
